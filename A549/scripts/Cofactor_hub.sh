bea7af8fec0ba7e3620c873265a5c9e85734c2b6
Eric Fournier <ericfournier2@yahoo.ca>
2017-06-22 13:56:14 -0400

    Add hub generation scripts.

---
+++ A549/scripts/Cofactor_hub.sh
@@ -0,0 +1,6 @@
+$MUGQIC_UTILS_PATH/mugqic_generate_chip_hub.sh \
+    -d chip-pipeline-GRCh38 -r fhq-091-aa -g hg38 \
+    -n A549_Cofactor_ChIP_hg38 \
+    --hublabelshort A549_Cofactor_ChIP_hg38 \
+    --hublabellong A549_Cofactor_ChIP_hg38 \
+    --huboptions input/chip-hub-options.txt
---
+++ A549/scripts/ENCODE_hub.sh
@@ -0,0 +1,6 @@
+$MUGQIC_UTILS_PATH/mugqic_generate_chip_hub.sh \
+    -d output/ENCODE-chip/ -r fhq-091-aa -g hg38 \
+    -n ENCODE_A549_GR \
+    --hublabelshort ENCODE_A549_GR \
+    --hublabellong ENCODE_A549_GR \
+    --huboptions input/ENCODE-chip-hub-options.txt
