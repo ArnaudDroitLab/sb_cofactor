50b075c730b9f5d53bc32caf0279f1c38e2a646f
Eric Fournier <ericfournier2@yahoo.ca>
2017-08-09 10:46:53 -0400

    Run rna-seq pipeline on GRCh38

--- A549/scripts/generate_pipeline.sh
+++ A549/scripts/generate_pipeline.sh
@@ -7,8 +7,8 @@ mkdir -p $PROJECT_BASE/output/rna-pipeline
 $MUGQIC_PIPELINES_HOME/pipelines/rnaseq/rnaseq.py -s '1-22' -l debug \
     -r $PROJECT_BASE/raw/rna-seq/readset.txt \
     -d $PROJECT_BASE/raw/rna-seq/design.txt \
-    -o $PROJECT_BASE/output/rna-pipeline \
+    -o $PROJECT_BASE/output/rna-pipeline-GRCh38 \
     --config $MUGQIC_PIPELINES_HOME/pipelines/rnaseq/rnaseq.base.ini \
         $MUGQIC_PIPELINES_HOME/pipelines/rnaseq/rnaseq.guillimin.ini \
-        $MUGQIC_INSTALL_HOME/genomes/species/Homo_sapiens.hg19/Homo_sapiens.hg19.ini \
+        $MUGQIC_INSTALL_HOME/genomes/species/Homo_sapiens.GRCh38/Homo_sapiens.GRCh38.ini \
         $PROJECT_BASE/input/this_run.ini
