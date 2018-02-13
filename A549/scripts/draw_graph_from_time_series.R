01e8d6b2e621fdb9b239a9e8b3a1ac9f5cab9de1
chriis11 <christophe.tav@gmail.com>
2018-02-13 16:11:11 -0500

    Remove two scripts: How to retrieve mapping files?

--- A549/scripts/draw_graph_from_time_series.R
+++ A549/scripts/draw_graph_from_time_series.R
@@ -90,3 +90,39 @@ draw_a549_dex_time_points <- function(genes_list, counts_mat, mapping) {
 test_genes <- c("BRD4", "HEXIM1", "NELFA", "NCOR1", "NCOR2", "TBL1XR1", "NR3C1", "NIPBL", "WNT7B", "FZD8", "HACD3", "FZD5", "DVL2")
 draw_a549_dex_time_points(test_genes, expressed_counts_norm, map_ens_hgnc)
 
+
+
+
+#### A INTEGRER
+
+
+source("https://bioconductor.org/biocLite.R")
+setwd("/home/chris/Bureau/A549_script")
+library(biomaRt)
+
+
+# How to retrieve mapping file: Ensemble ID - HGNC symbol
+
+listMarts()    # to see which database options are present
+
+ensembl=useMart("ensembl")  # using ensembl database data
+
+listDatasets(ensembl)     # function to see which datasets are present in ensembl
+
+ensembl=useDataset("hsapiens_gene_ensembl", mart=ensembl)   # from ensembl using homosapien gene data
+
+listFilters(ensembl)  # check which filters are available
+
+listAttributes(ensembl) # check attributes are available to select.More information on ensembl data base
+# 
+# genes.with.id=getBM(attributes=c("ensembl_gene_id", "external_gene_id"),values=gene_names, mart= ensembl) # function to get  gene id's and gene name from data base
+#
+mapping <- getBM(attributes=c("ensembl_gene_id", "hgnc_symbol"), mart=ensembl)
+mapping[mapping == ""] <- NA
+
+nb_NA <- sum(is.na(mapping$hgnc_symbol))
+
+#  22582 NAs ==» 35.26839 % of ENSEMBL IDS has no SYMBOLs
+# total: 64029 ENSEMBL IDS
+
+write.csv(mapping, file="map_ensembl_hgnc.csv", row.names=FALSE, quote=FALSE)
