f4bf8cfe1ac8399c40715a1e3ee5bf93156420cf
Eric Fournier <ericfournier2@yahoo.ca>
2018-03-13 08:23:00 -0700

    Add scripts used to move alignment files from other pipeline run.

---
+++ A549/scripts/move_alignments.sh
@@ -0,0 +1,30 @@
+OLD_OUTPUT=/home/efournie/projects/def-stbil30/Working_Directory/Eric/A549_Michele/output/chip-pipeline-GRCh38
+NEW_OUTPUT=/home/efournie/projects/def-stbil30/Working_Directory/Eric/CofactorHR/A549/output/chip-pipeline-GRCh38
+
+OLD_PREFIX=BRD4_NA_EtOH_Rep2
+NEW_PREFIX=A549_CTRL_BRD4_rep2
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bai       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bai
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam.flagstat  $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam.flagstat
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.metrics   $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.metrics
+
+OLD_PREFIX=BRD4_NA_Dex_Rep2
+NEW_PREFIX=A549_DEX_BRD4_rep2
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bai       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bai
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam.flagstat  $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam.flagstat
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.metrics   $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.metrics
+
+OLD_PREFIX=CDK9_NA_EtOH_Rep2
+NEW_PREFIX=A549_CTRL_CDK9_rep2
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bai       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bai
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam.flagstat  $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam.flagstat
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.metrics   $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.metrics
+
+OLD_PREFIX=CDK9_NA_Dex_Rep2
+NEW_PREFIX=A549_DEX_CDK9_rep2
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bai       $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bai
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.bam.flagstat  $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.bam.flagstat
+mv $OLD_OUTPUT/alignment/$OLD_PREFIX/$OLD_PREFIX.sorted.dup.metrics   $NEW_OUTPUT/alignment/$NEW_PREFIX/$NEW_PREFIX.sorted.dup.metrics
