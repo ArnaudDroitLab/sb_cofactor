3a57013cf0141d4a4e6a186d64b32a8a5f914dee
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2018-04-03 15:57:00 -0400

    Perform actual GR enrichment at all time points.

--- A549/scripts/gr_enrichment.R
+++ A549/scripts/gr_enrichment.R
@@ -2,6 +2,13 @@ library(ENCODExplorer)
 library(ef.utils)
 library(GenomicRanges)
 library(ggplot2)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(ChIPseeker)
+library(org.Hs.eg.db)
+library(reshape2)
+
+output_dir="output/analyses/gr_enrichment"
+dir.create(output_dir, recursive=TRUE, showWarnings=FALSE)
 
 # Get GR binding time series
 gr_accession = read.table("input/ENCODE_Reddy_GR_ChIP_experiments.txt", header=TRUE, sep="\t")
@@ -27,7 +34,10 @@ for(i in 1:nrow(gr_accession)) {
     intersect_object = build_intersect(binding_sites)
     two_replicates = rowSums(intersect_object$Matrix) >= 2
     gr_regions[[time_point]] = intersect_object$Regions[two_replicates]
-    intersect_venn_plot(intersect_object, paste0(time_point, ".tiff"))
+    intersect_venn_plot(intersect_object, file.path(output_dir, paste0(time_point, ".tiff")))
+
+    # Get rid of incomplete/alternate scaffolds, since they interfere with annotation later.
+    gr_regions[[time_point]] = gr_regions[[time_point]][!grepl("_", seqnames(gr_regions[[time_point]]))]
 }
     
 # Plot the number of binding sites over time.
@@ -38,12 +48,14 @@ ggplot(binding_over_time, aes(x=Time, y=NumberOfRegions)) +
     labs(x="Time", y="Number of identified GR-binding sites", title="GR-binding over time in Reddy dataset") +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
-ggsave("GR-binding over time in Reddy dataset.pdf")
+ggsave(file.path(output_dir, "GR-binding over time in Reddy dataset.pdf"))
 
 # Are binding regions conserved across time points?
 intersect_all = build_intersect(GRangesList(gr_regions))
+all_regions_annotation = ChIPseeker::annotatePeak(intersect_all$Regions, TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene)
+all_gr_regions = GRanges(as.data.frame(all_regions_annotation))
 
-pdf("Heatmap of GR-binding sites.pdf")    
+pdf(file.path(output_dir, "Heatmap of GR-binding sites.pdf"))
 heatmap(intersect_all$Matrix, Rowv=NA, Colv=NULL)
 dev.off()    
     
@@ -53,6 +65,7 @@ for(de_result_file in Sys.glob("results/a549_dex_time_points/*h")) {
     time_point = gsub(".*points\\/(.*h)$", "\\1", de_result_file)
     de_results[[time_point]] = list()
     de_results[[time_point]]$Full = read.csv(de_result_file)
+    de_results[[time_point]]$Full$ENTREZID = mapIds(org.Hs.eg.db, keys=as.character(de_results[[time_point]]$Full$gene_id), keytype="ENSEMBL", column="ENTREZID")
     de_results[[time_point]]$DE_indices = with(de_results[[time_point]]$Full, abs(log2FoldChange) > log2(1.5) & padj < 0.05)
     de_results[[time_point]]$DE = de_results[[time_point]]$Full[de_results[[time_point]]$DE_indices,]
     
@@ -66,14 +79,17 @@ for(de_result_file in Sys.glob("results/a549_dex_time_points/*h")) {
 # Plot the number of DE genes per time point.
 de_over_time = data.frame(Time=names(de_results))
 de_over_time$Time = factor(de_over_time$Time, levels=c("0.5h", "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "10h", "12h"))
-de_over_time$NumberOfGenes = unlist(lapply(de_results, function(x) { sum(x$DE_indices, na.rm=TRUE)}))
+#de_over_time$DE = unlist(lapply(de_results, function(x) { sum(x$DE_indices, na.rm=TRUE)}))
+de_over_time$Up = unlist(lapply(de_results, function(x) { sum(x$Up_indices, na.rm=TRUE)}))
+de_over_time$Down = unlist(lapply(de_results, function(x) { sum(x$Down_indices, na.rm=TRUE)}))
+de_over_time = melt(de_over_time, variable.name="Direction", value.name="NumberOfGenes")
 
-ggplot(de_over_time, aes(x=Time, y=NumberOfGenes)) + 
-    geom_bar(stat="identity", color="black", fill="#4FC3F7") +
+ggplot(de_over_time, aes(x=Time, y=NumberOfGenes, fill=Direction)) + 
+    geom_bar(stat="identity", color="black") +
     labs(x="Time", y="Number of identified DE genes", title="Differential expression over time in Reddy dataset") +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
-ggsave("DE over time in Reddy dataset.pdf")
+ggsave(file.path(output_dir, "DE over time in Reddy dataset.pdf"))
 
 
 # Make a matrix of fold-changes so we can plot a heatmap.
@@ -88,9 +104,54 @@ for(de_item in names(de_results)) {
     all_fc[[de_item]] = de_results[[de_item]]$Full$log2FoldChange[match(all_fc$gene_id, de_results[[de_item]]$Full$gene_id)]
 }   
 
-pdf("DE heatmap.pdf")
+pdf(file.path(output_dir, "DE heatmap.pdf"))
 heatmap(as.matrix(all_fc[,-1]), Rowv=NA, Colv=NULL)
 dev.off()
     
 # Perform enrichment of GR binding
+time_matches = read.table("input/TimeMatches.txt", sep="\t", header=TRUE)
+enrich_df = data.frame()
+for(de_index in 1:nrow(time_matches)) {
+    for(direction in c("Up", "Down")) {
+        de_time = time_matches$DETime[de_index]
+        gr_time = time_matches$HourBefore[de_index]
+        
+        # Get total number of genes.
+        n_total_genes = length(unique(de_results[[de_time]]$Full$ENTREZID))
+        
+        # Get the number of DEregulated genes.
+        de_genes = de_results[[de_time]][[direction]]$ENTREZID
+        n_de_genes = length(unique(de_genes))
+        
+        # Get the list of GR-bound genes.
+        gr_ranges = all_gr_regions[intersect_all$List[[gr_time]]]
+        gr_promoters = gr_ranges[gr_ranges$annotation=="Promoter (<=1kb)"]
+        
+        # Get the total number of GR-bound genes
+        gr_genes = gr_promoters$geneId
+        n_gr_genes = length(unique(gr_genes))
+        
+        # Get the total number of GR-bound de-regulated genes.
+        n_gr_de_genes = length(unique(intersect(de_genes, gr_genes)))
+        
+        # Do hyper geometric enrichment.
+        enrichment_pval = phyper(n_gr_de_genes, n_gr_genes, n_total_genes - n_gr_genes, n_de_genes, lower.tail=FALSE)
+        
+        row_df = data.frame(Direction=direction,
+                            DETime=de_time,
+                            GRTime=gr_time,
+                            DEGenes = n_de_genes,
+                            GRGenes = n_gr_genes,
+                            GRDEGenes = n_gr_de_genes,
+                            TotalGenes = n_total_genes, 
+                            DEProportion = n_de_genes / n_total_genes,
+                            GRProportionAll = n_gr_genes / n_total_genes,
+                            GRProportionDE = n_gr_de_genes / n_de_genes,
+                            PVal=enrichment_pval)
+                            
+        enrich_df = rbind(enrich_df, row_df)
+    }
+}
     
+enrich_df$Enrichment = log2(enrich_df$GRProportionDE / enrich_df$GRProportionAll)
+write.table(enrich_df, row.names=FALSE, col.names=TRUE, sep="\t", file=file.path(output_dir, "Enrichment.txt"))
