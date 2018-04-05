52ccc2afaf41d1ddbd0c0f68a97893adc08d672c
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2018-04-05 11:15:02 -0400

    Plot types of GR binding peaks.

--- A549/scripts/gr_enrichment.R
+++ A549/scripts/gr_enrichment.R
@@ -6,9 +6,18 @@ library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(ChIPseeker)
 library(org.Hs.eg.db)
 library(reshape2)
+library(dplyr)
+library(gplots)
+
+# Turn off VennDiagram logging.
+futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
 
 output_dir="output/analyses/gr_enrichment"
 dir.create(output_dir, recursive=TRUE, showWarnings=FALSE)
+
+###############################################################################
+# Load and annotate GR binding data.
+###############################################################################
 
 # Get GR binding time series
 gr_accession = read.table("input/ENCODE_Reddy_GR_ChIP_experiments.txt", header=TRUE, sep="\t")
@@ -34,26 +43,22 @@ for(i in 1:nrow(gr_accession)) {
     intersect_object = build_intersect(binding_sites)
     two_replicates = rowSums(intersect_object$Matrix) >= 2
     gr_regions[[time_point]] = intersect_object$Regions[two_replicates]
-    intersect_venn_plot(intersect_object, file.path(output_dir, paste0(time_point, ".tiff")))
+    dir.create(file.path(output_dir, "Venn diagrams of peak calls"))
+    intersect_venn_plot(intersect_object, file.path(output_dir, "Venn diagrams of peak calls", paste0(time_point, ".tiff")))
 
     # Get rid of incomplete/alternate scaffolds, since they interfere with annotation later.
     gr_regions[[time_point]] = gr_regions[[time_point]][!grepl("_", seqnames(gr_regions[[time_point]]))]
 }
     
-# Plot the number of binding sites over time.
-binding_over_time = data.frame(NumberOfRegions=unlist(lapply(gr_regions, length)),
-                               Time=factor(names(gr_regions), levels=gr_accession$Time))
-ggplot(binding_over_time, aes(x=Time, y=NumberOfRegions)) + 
-    geom_bar(stat="identity", color="black", fill="#4FC3F7") +
-    labs(x="Time", y="Number of identified GR-binding sites", title="GR-binding over time in Reddy dataset") +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
-ggsave(file.path(output_dir, "GR-binding over time in Reddy dataset.pdf"))
 
 # Are binding regions conserved across time points?
 intersect_all = build_intersect(GRangesList(gr_regions))
 all_regions_annotation = ChIPseeker::annotatePeak(intersect_all$Regions, TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene)
 all_gr_regions = GRanges(as.data.frame(all_regions_annotation))
+
+###############################################################################
+# Plot some basic metrics of the GR binding data.
+###############################################################################
 
 pdf(file.path(output_dir, "Heatmap of GR-binding sites.pdf"))
 heatmap.2(intersect_all$Matrix, dendrogram="column", scale="none", trace="none")
@@ -64,18 +69,37 @@ gr_binding_type_df = data.frame()
 for(i in names(intersect_all$List)) {
     which_regions = all_gr_regions[intersect_all$List[[i]]]
     row_df = data.frame(Time=i,
-                        Promoter=sum(which_regions$annotation=="Promoter (<=1kb)"),
-                        Other=sum(which_regions$annotation!="Promoter (<=1kb)"))
+                        Annotation=gsub(" \\(.*\\)", "", which_regions$annotation))
     gr_binding_type_df = rbind(gr_binding_type_df, row_df)                    
                         
 }
-gr_binding_type_df = melt(gr_binding_type_df, id.vars="Time", variable.name="BindingLocation", value.name="NumberOfRegions")    
 gr_binding_type_df$Time = factor(gr_binding_type_df$Time, levels=gr_accession$Time)
 
-ggplot(gr_binding_type_df, aes(x=Time, y=NumberOfRegions, fill=BindingLocation)) +
-    geom_bar(stat="identity", color="black")
+# Plot GR binding type in absolute number of regions.
+ggplot(gr_binding_type_df, aes(x=Time, fill=Annotation)) +
+    geom_bar(color="black") +
+    labs(x="Time", y="Number of identified GR-binding sites", title="GR-binding over time in Reddy dataset") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
+ggsave(file.path(output_dir, "GR-binding over time in Reddy dataset (absolute).pdf"))
     
+# Plot GR binding type as proportion of current GR-bindings.
+gr_proportions = gr_binding_type_df %>% 
+                    group_by(Time, Annotation) %>% 
+                    summarise(n=n()) %>% 
+                    mutate(freq = n / sum(n))
+ggplot(gr_proportions, aes(x=Time, fill=Annotation, y=freq*100)) +
+    geom_bar(color="black", stat="identity") +
+    labs(x="Time", y="Percentage of identified GR-binding sites", title="GR-binding over time in Reddy dataset") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
+ggsave(file.path(output_dir, "GR-binding over time in Reddy dataset (proportions).pdf"))
+
+###############################################################################
 # Load DE results
+###############################################################################
+
+ # Loop over all result files.
 de_results = list()
 for(de_result_file in Sys.glob("results/a549_dex_time_points/*h")) {
     time_point = gsub(".*points\\/(.*h)$", "\\1", de_result_file)
@@ -92,10 +116,13 @@ for(de_result_file in Sys.glob("results/a549_dex_time_points/*h")) {
     de_results[[time_point]]$Down = de_results[[time_point]]$Full[de_results[[time_point]]$Down_indices,]    
 }    
 
+###############################################################################
+# Plot simple DE metrics.
+###############################################################################
+
 # Plot the number of DE genes per time point.
 de_over_time = data.frame(Time=names(de_results))
 de_over_time$Time = factor(de_over_time$Time, levels=c("0.5h", "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "10h", "12h"))
-#de_over_time$DE = unlist(lapply(de_results, function(x) { sum(x$DE_indices, na.rm=TRUE)}))
 de_over_time$Up = unlist(lapply(de_results, function(x) { sum(x$Up_indices, na.rm=TRUE)}))
 de_over_time$Down = unlist(lapply(de_results, function(x) { sum(x$Down_indices, na.rm=TRUE)}))
 de_over_time = melt(de_over_time, variable.name="Direction", value.name="NumberOfGenes")
@@ -107,19 +134,15 @@ ggplot(de_over_time, aes(x=Time, y=NumberOfGenes, fill=Direction)) +
     theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1), plot.title = element_text(hjust=0.5))
 ggsave(file.path(output_dir, "DE over time in Reddy dataset.pdf"))
 
-
 # Make a matrix of fold-changes so we can plot a heatmap.
-all_de = c()
-for(de_item in de_results) {
-    all_de = c(all_de, as.character(de_item$DE$gene_id))
-}
-all_de = unique(all_de)
-   
+all_de = unique(unlist(lapply(de_results, function(x) { c(x[["Up"]]$gene_id, x[["Down"]]$gene_id) })))
 all_fc = data.frame(gene_id=all_de)
 for(de_item in names(de_results)) {
-    all_fc[[de_item]] = de_results[[de_item]]$Full$log2FoldChange[match(all_fc$gene_id, de_results[[de_item]]$Full$gene_id)]
+    cur_fc = de_results[[de_item]]$Full
+    all_fc[[de_item]] = cur_fc$log2FoldChange[match(all_fc$gene_id, cur_fc$gene_id)]
 }   
 
+# Remove NAs, and cap FC values at +/-5 to keep the color scale more useful.
 heatmap_matrix = as.matrix(all_fc[,-1])
 heatmap_matrix[is.na(heatmap_matrix)] = 0
 heatmap_matrix[heatmap_matrix < -5] = -5
@@ -132,7 +155,11 @@ pdf(file.path(output_dir, "DE heatmap.pdf"))
 heatmap.2(heatmap_matrix, dendrogram="column", scale="none", trace="none", col=color_palette, breaks=color_breaks)
 dev.off()
     
-# Perform enrichment of GR binding
+
+###############################################################################
+# Perform enrichment of GR binding.
+############################################################################    
+    
 time_matches = read.table("input/TimeMatches.txt", sep="\t", header=TRUE)
 enrich_df = data.frame()
 for(de_index in 1:nrow(time_matches)) {
