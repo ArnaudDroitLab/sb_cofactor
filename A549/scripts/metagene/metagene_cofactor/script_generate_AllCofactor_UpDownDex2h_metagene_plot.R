366586d0ef1e3825190ba36a76519b6724c7c851
ckntav <christophe.tav@gmail.com>
2018-04-23 16:06:47 -0400

    Generate plots

--- A549/scripts/metagene/metagene_cofactor/script_generate_AllCofactor_UpDownDex2h_metagene_plot.R
+++ A549/scripts/metagene/metagene_cofactor/script_generate_AllCofactor_UpDownDex2h_metagene_plot.R
@@ -9,56 +9,44 @@ library(wesanderson)
 
 cofactor_list <- c("BRD4", "CDK9", "NIPBL", "SMC1A", "MED1")
 
-bigdf <- data.frame(region = character(),
-				   design = character(),
-				   bin = integer(),
-				   value = double(),
-				   strand = character(),
-				   qinf = double(),
-				   qsup = double(),
-				   group = factor(),
-				   Condition = factor(),
-				   Replicate = factor(),
-				   Cofactor = factor())
+output_dir <- "output/chip-pipeline-GRCh38/metagene/metagene_cofactor"
 
-output_dir <- "output/chip-pipeline-GRCh38/metagene_cofactor"
+colorReplicate = wes_palette(n=2, name="Cavalcanti1")
 
 for (cofactor in cofactor_list) {
-	metagene_obj_name <- file.path(output_dir, paste0(cofactor, regions_names, "_metagene_obj.RData"))
+	metagene_obj_name <- file.path(output_dir, paste0(cofactor, "_UpRegulatedTSS_DownRegulatedTSS", "_metagene_obj.RData"))
 	load(metagene_obj_name)
 	
 	mg_df <- mg$get_data_frame()
 	mg_df$Condition <- as.factor(ifelse(grepl("DEX", mg_df$group), "DEX", "EtOH"))
 	mg_df$Replicate <- as.factor(ifelse(grepl("rep1", mg_df$group), "Replicate 1", "Replicate 2"))
 	mg_df$Cofactor <- as.factor(cofactor)
+	mg_df$Direction <- as.factor(ifelse(grepl("UpRegulatedTSS", mg_df$group), "UpRegulatedTSS", "DownRegulatedTSS"))
 	
-	bigdf <- rbind(bigdf, mg_df)
+	title <- paste0(cofactor, "_UpRegulatedTSS_DownRegulatedTSS", "_DEX_2hours")
+	
+	ggplot(mg_df, aes(x=bin, y=value, ymin=qinf, ymax=qsup, group=Replicate)) +
+		geom_ribbon(aes(fill = Replicate), alpha = 0.3) +
+		scale_fill_manual(values=colorReplicate) +
+		geom_line(aes(color = Replicate), size = 1) + 
+		scale_color_manual(values=colorReplicate) +
+		theme_bw(base_size = 20) +
+		theme(panel.grid.major = element_line(),
+			  panel.grid.minor = element_line(),
+			  panel.background = element_blank()) +
+		theme(legend.position = "bottom",
+			  legend.direction = "vertical") +
+		scale_x_continuous(breaks = seq(0, 200, 50),
+						   labels = c(c(-0.5, -0.25), "TSS", c(0.25, 0.5))) +
+		theme(axis.text.x = element_text(size=10),
+			  axis.text.y = element_text(size=10)) +
+		ggtitle(title) +
+		xlab("Position from TSS (kb)") +
+		ylab("Mean coverage (RPM)") +
+		facet_grid(Cofactor ~ Direction*Condition)
+	
+	output_filename <- file.path(output_dir, paste0(cofactor, "_UpRegulatedTSS_DownRegulatedTSS_w1000_metagene.pdf"))
+	
+	ggsave(output_filename, width = 14, height = 7, units = "in")
+	
 }
-
-colorReplicate = wes_palette(n=2, name="Cavalcanti1")
-
-title <- paste0("AllTSS regions")
-
-ggplot(bigdf, aes(x=bin, y=value, ymin=qinf, ymax=qsup, group=Replicate)) +
-	geom_ribbon(aes(fill = Replicate), alpha = 0.3) +
-	scale_fill_manual(values=colorReplicate) +
-	geom_line(aes(color = Replicate), size = 1) + 
-	scale_color_manual(values=colorReplicate) +
-	theme_bw(base_size = 20) +
-	theme(panel.grid.major = element_line(),
-		  panel.grid.minor = element_line(),
-		  panel.background = element_blank()) +
-	theme(legend.position = "bottom",
-		  legend.direction = "vertical") +
-	scale_x_continuous(breaks = seq(0, 100, 25),
-					   labels = c(c(-1.5, -0.75), "TSS", c(0.75, 1.5))) +
-	theme(axis.text.x = element_text(size=10),
-		  axis.text.y = element_text(size=10)) +
-	ggtitle(title) +
-	xlab("Position from TSS (kb)") +
-	ylab("Mean coverage (RPM)") +
-	facet_grid(Cofactor ~ Condition)
-
-output_filename <- file.path(output_dir, "AllCofactor_AllTSS_w3000_metagene.pdf")
-
-ggsave(output_filename, width = 7, height = 14, units = "in")
