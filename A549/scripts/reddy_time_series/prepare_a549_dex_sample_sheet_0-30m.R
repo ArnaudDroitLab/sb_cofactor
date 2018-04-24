6a4d8bbcda3bf0fe2ecdec6b654494f22e31227a
chriis11 <christophe.tav@gmail.com>
2018-04-24 10:30:14 -0400

    Add script to generate sample sheet time series 0-30m.csv

---
+++ A549/scripts/reddy_time_series/prepare_a549_dex_sample_sheet_0-30m.R
@@ -0,0 +1,18 @@
+library(ENCODExplorer)
+library(tidyverse)
+
+time_point <- c("0m", "5m", "10m",
+                "15m", "20m", "25m")
+
+accession <- c("ENCSR937WIG", "ENCSR482TZY", "ENCSR742VGF",
+               "ENCSR964GKZ", "ENCSR831FZM", "ENCSR525HSH")
+
+design <- data.frame(accession = accession, time_point = time_point)
+
+sample_sheet <- dplyr::filter(encode_df, accession %in% design$accession) %>%
+    dplyr::filter(file_type == "tsv") %>%
+    dplyr::select(accession, file_accession, href) %>%
+    mutate(href = paste0("https://www.encodeproject.org", href)) %>%
+    left_join(design, by = "accession")
+
+write_csv(sample_sheet, "input/sample_sheet_time_series_0-30m.csv")
