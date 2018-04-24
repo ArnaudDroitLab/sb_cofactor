95b3f1c4f67ddd1b245c4e3cbbcdfda807bfd5ad
chriis11 <christophe.tav@gmail.com>
2018-04-24 10:33:40 -0400

    Add script to retrieve counts from tsv file related to each replicate

---
+++ A549/scripts/reddy_time_series/import_quantification_time_series_0-30m.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(stringr)
+library(tools)
+
+files <- dir("input", pattern = "ENCFF", full.names = TRUE)
+
+import_counts <- function(filename) {
+    n <- file_path_sans_ext(filename) %>% basename
+    suppressMessages(x <- read_tsv(filename, skip = 1))
+    colnames(x) <- c("gene_id", "chr", "start", "end", "strand", "length", "count")
+    x <- dplyr::select(x, gene_id, count) %>%
+        mutate(gene_id = str_replace(gene_id, "\\.[0-9]+$", ""))
+    colnames(x) <- c("gene_id", n)
+    x
+}
+raw_counts <- Reduce("left_join", map(files, import_counts))
+dir_results <- "results/a549_dex_time_points_0-30m"
+write_csv(raw_counts, paste0(dir_results, "/raw_counts_II.csv"))
