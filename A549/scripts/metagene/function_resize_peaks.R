4a6a468f6a0b4c89c62669297c920697c7348222
ckntav <christophe.tav@gmail.com>
2018-05-08 15:48:17 -0400

    R function to resize peaks

---
+++ A549/scripts/metagene/function_resize_peaks.R
@@ -0,0 +1,21 @@
+
+resize_all_peaks <- function(granges.obj, window) {
+	ranges(granges.obj) <- resize(ranges(granges.obj), width = window, fix="center")
+	return (granges.obj)
+}
+
+resize_broad_peaks <- function(granges.obj, window) {
+	l <- length(granges.obj)
+	message("Number of regions : ", l)
+	for (i in seq(1, l)) {
+		w <- width(ranges(granges.obj[i]))
+		if (w > window) {
+			ranges(granges.obj[i]) <- resize(ranges(granges.obj[i]), width = window, fix="center")
+			message("Region number ", i, " / ", l, " : ", w, " nt", " --» resized to 500 nt")
+		}
+		else {
+			message("Region number ", i, " / ", l, " : ", w, " nt")
+		}
+	}
+	return (granges.obj)
+}
