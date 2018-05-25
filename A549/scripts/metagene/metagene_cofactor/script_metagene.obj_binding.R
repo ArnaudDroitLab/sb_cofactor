a54c1ead615df8ec9bea43bdc9dfa9b52fcefe7c
ckntav <christophe.tav@gmail.com>
2018-05-25 13:22:45 -0400

    plot script ok

--- A549/scripts/metagene/metagene_cofactor/script_metagene.obj_binding.R
+++ A549/scripts/metagene/metagene_cofactor/script_metagene.obj_binding.R
@@ -1,3 +1,5 @@
+# setwd("/home/chris/Bureau/sb_cofactor_hr/A549/")
+
 library(knitr)
 library(dplyr)
 library(org.Hs.eg.db)
@@ -7,7 +9,7 @@ library(rtracklayer)
 source("scripts/metagene/function_generate_metagene_object.R")
 source("scripts/metagene/function_generate_WCE_metagene_object.R")
 source("scripts/metagene/function_resize_peaks.R")
-source("scripts/load_reddy.R")
+# source("scripts/load_reddy.R")
 
 ########################################
 # Define directory
@@ -20,7 +22,7 @@ binding_diff_dir <- "output/chip-pipeline-GRCh38/binding_diff"
 ###############################################################################
 # cofactor_list <- c("BRD4", "CDK9", "NIPBL", "SMC1A", "MED1")
 
-cofactor_list <- c("BRD4", "CDK9")
+cofactor_list <- c("BRD4")
 	
 for (cofactor in cofactor_list) {
 	message("##########     ", cofactor, "     ##########")
@@ -31,21 +33,21 @@ for (cofactor in cofactor_list) {
 	# Define regions over which metagenes will be plotted.
 	###############################################################################
 	
-	CstRegions_path <- file.path(output_dir, paste0("A549_", cofactor, "_unbiased_peaks.bed"))
+	# CstRegions_path <- file.path(output_dir, paste0("A549_", cofactor, "_unbiased_peaks.bed"))
 	BindUpRegions_path <- file.path(output_dir, paste0("A549_DEX_", cofactor, "_rep1_peaks.narrowPeak_M_above_1.0_biased_peaks.bed"))
 	BindDownRegions_path <- file.path(output_dir, paste0("A549_CTRL_", cofactor, "_rep1_peaks.narrowPeak_M_below_-1.0_biased_peaks.bed"))
 	
-	CstRegions <- rtracklayer::import(CstRegions_path)
+	# CstRegions <- rtracklayer::import(CstRegions_path)
 	BindUpRegions <- rtracklayer::import(BindUpRegions_path)
 	BindDownRegions <- rtracklayer::import(BindDownRegions_path)
 	
-	region_list = list(CstRegions = CstRegions,
+	region_list = list(#CstRegions = CstRegions,
 					   BindUpRegions = BindUpRegions,
 					   BindDownRegions = BindDownRegions)
 	
 	###############################################################################
 	# Generate the metagene object.
 	###############################################################################
-	generate_WCE_metagene_object(cofactor, region_list, bin=200)
+	# generate_WCE_metagene_object(cofactor, region_list, bin=200)
 	generate_metagene_object(cofactor, region_list, bin=200)
 }
