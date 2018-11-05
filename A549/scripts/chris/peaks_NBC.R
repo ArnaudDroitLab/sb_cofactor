9e7cdd884184a99fb961500a7b80f8d3d4caede6
Christophe Tav <christophe.tav@gmail.com>
2018-11-05 16:36:00 -0500

    set of peaks are in peak_call/A549_NBC

--- A549/scripts/chris/peaks_NBC.R
+++ A549/scripts/chris/peaks_NBC.R
@@ -18,19 +18,26 @@ NBC_CTRL <- subsetByOverlaps(NB_CTRL, CDK9_CTRL) # 5566
 NB_DEX <- subsetByOverlaps(NIPBL_DEX, BRD4_DEX) # 2634
 NBC_DEX <- subsetByOverlaps(NB_DEX, CDK9_DEX) # 2225
 ############
+ovCTRLvsDEX <- subsetByOverlaps(NBC_CTRL, NBC_DEX) # 1390
+summary(width(ovCTRLvsDEX))
+ovDEXvsCTRL <- subsetByOverlaps(NBC_DEX, NBC_CTRL) # 1417
+summary(width(ovDEXvsCTRL))
 
-subsetByOverlaps(NBC_CTRL, NBC_DEX)
-subsetByOverlaps(NBC_DEX, NBC_CTRL)
+ov <- sort(reduce(c(ovCTRLvsDEX, ovDEXvsCTRL))) # 1376
+summary(width(ov))
 
-
+speCTRL <- sort(setdiff(NBC_CTRL, ovCTRLvsDEX)) # 4176
+summary(width(speCTRL))
+speDEX <- sort(setdiff(NBC_DEX, ovDEXvsCTRL)) # 808
+summary(width(speDEX))
 
 peaks_NBC_dir <- "output/chip-pipeline-GRCh38/peak_call/A549_NBC"
 dir.create(peaks_NBC_dir, recursive=TRUE, showWarnings=FALSE)
 
-commonNIPBL_path <- file.path(peaks_NIPBL_dir, "A549_NIPBL_common.bed")
-speNIPBL_CTRL_path <- file.path(peaks_NIPBL_dir, "A549_NIPBL_CTRL_specific.bed")
-speNIPBL_DEX_path <- file.path(peaks_NIPBL_dir, "A549_NIPBL_DEX_specific.bed")
+commonNBC_path <- file.path(peaks_NBC_dir, "A549_NBC_common.bed")
+speNBC_CTRL_path <- file.path(peaks_NBC_dir, "A549_NBC_CTRL_specific.bed")
+speNBC_DEX_path <- file.path(peaks_NBC_dir, "A549_NBC_DEX_specific.bed")
 
-rtracklayer::export.bed(ov, con = commonNIPBL_path, format = "bed")
-rtracklayer::export.bed(speCTRL, con = speNIPBL_CTRL_path, format = "bed")
-rtracklayer::export.bed(speDEX, con = speNIPBL_DEX_path, format = "bed")
+rtracklayer::export.bed(ov, con = commonNBC_path, format = "bed")
+rtracklayer::export.bed(speCTRL, con = speNBC_CTRL_path, format = "bed")
+rtracklayer::export.bed(speDEX, con = speNBC_DEX_path, format = "bed")
