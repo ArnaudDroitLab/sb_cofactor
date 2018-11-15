7f394b5fd4a8a1973e6afc4c905d1e268a35fc50
chriis11 <christophe.tav@gmail.com>
2018-11-15 17:47:06 -0500

    update countReads_perRegions

--- A549/scripts/chris/countReads_perRegion.R
+++ A549/scripts/chris/countReads_perRegion.R
@@ -1,47 +1,42 @@
 setwd("/home/chris/Bureau/sb_cofactor_hr/A549")
 source("scripts/ckn_utils.R")
 
+#####
+generate_coordVector <- function(granges) {
+  df <- as.data.frame(granges)
+  coord_Vector <- paste0(df$seqnames, ":", df$start, "-", df$end)
+  return (coord_Vector)
+}
+
+generate_bampathVector <- function(cofactors, conditions) {
+  bampath <- c()
+  bam_dir <- "/home/chris/Bureau/sb_cofactor_hr/A549/output/chip-pipeline-GRCh38/alignment"
+  for (condition in conditions) {
+    for (cofactor in cofactors) {
+      basename <- paste0("A549", "_", condition, "_", cofactor, "_rep1")
+      path <- file.path(bam_dir, basename, paste0(basename, ".sorted.dup.bam"))
+      bampath <- c(bampath, path)
+    }
+  }
+  return(bampath)
+}
+#####
+bam_path <- generate_bampathVector(cofactors = c("NIPBL", "BRD4", "CDK9"),
+                                   conditions = c("CTRL", "DEX"))
+
 peaks_NBC_dir <- "output/chip-pipeline-GRCh38/peak_call/A549_NBC"
 
 speNBC_CTRL_path <- file.path(peaks_NBC_dir, "A549_NBC_CTRL_specific.bed")
 speNBC_CTRL <- rtracklayer::import(speNBC_CTRL_path)
 
-cofactors_peaks <- load_cofactor_stdchr_peaks()
+speNBC_CTRL_coordVector <- generate_coordVector(speNBC_CTRL)
 
-NIPBL_CTRL <- cofactors_peaks[["NIPBL_CTRL"]] # 9470
-BRD4_CTRL <- cofactors_peaks[["BRD4_CTRL"]] # 28084
-CDK9_CTRL <- cofactors_peaks[["CDK9_CTRL"]] # 10788
+# samtools view /home/chris/Bureau/sb_cofactor_hr/A549/output/chip-pipeline-GRCh38/alignment/A549_CTRL_NIPBL_rep1/A549_CTRL_NIPBL_rep1.sorted.dup.bam chr12:104290274-104290284
 
-NIPBL_DEX <- cofactors_peaks[["NIPBL_DEX"]] # 2733
-BRD4_DEX <- cofactors_peaks[["BRD4_DEX"]] # 21225
-CDK9_DEX <- cofactors_peaks[["CDK9_DEX"]] # 6061
-
-NB_CTRL <- subsetByOverlaps(NIPBL_CTRL, BRD4_CTRL) # 8273
-NBC_CTRL <- subsetByOverlaps(NB_CTRL, CDK9_CTRL) # 5566
-
-NB_DEX <- subsetByOverlaps(NIPBL_DEX, BRD4_DEX) # 2634
-NBC_DEX <- subsetByOverlaps(NB_DEX, CDK9_DEX) # 2225
-############
-ovCTRLvsDEX <- subsetByOverlaps(NBC_CTRL, NBC_DEX) # 1390
-summary(width(ovCTRLvsDEX))
-ovDEXvsCTRL <- subsetByOverlaps(NBC_DEX, NBC_CTRL) # 1417
-summary(width(ovDEXvsCTRL))
-
-ov <- sort(reduce(c(ovCTRLvsDEX, ovDEXvsCTRL))) # 1376
-summary(width(ov))
-
-speCTRL <- sort(setdiff(NBC_CTRL, ovCTRLvsDEX)) # 4176
-summary(width(speCTRL))
-speDEX <- sort(setdiff(NBC_DEX, ovDEXvsCTRL)) # 808
-summary(width(speDEX))
-
-
-dir.create(peaks_NBC_dir, recursive=TRUE, showWarnings=FALSE)
-
-commonNBC_path <- file.path(peaks_NBC_dir, "A549_NBC_common.bed")
-
-speNBC_DEX_path <- file.path(peaks_NBC_dir, "A549_NBC_DEX_specific.bed")
-
-# rtracklayer::export.bed(ov, con = commonNBC_path, format = "bed")
-# rtracklayer::export.bed(speCTRL, con = speNBC_CTRL_path, format = "bed")
-# rtracklayer::export.bed(speDEX, con = speNBC_DEX_path, format = "bed")
+# function: input: GRranges, output: vector name of region
+# system samtools
+# samtools view bam region | wc -l
+# output: pour chaque set de regions, tableau de counts avec les 6 samples
+# faire une PCA?
+# correlation entre ces deux listes de chiffres
+# faire une PCA
