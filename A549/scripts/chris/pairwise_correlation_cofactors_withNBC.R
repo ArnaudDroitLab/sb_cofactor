561ab5de5323c64f13bfce2d35ea7343cf1e85a7
chris11 <christophe.tav@gmail.com>
2018-11-23 16:35:30 -0500

    need to do the data viz

--- A549/scripts/chris/pairwise_correlation_cofactors_withNBC.R
+++ A549/scripts/chris/pairwise_correlation_cofactors_withNBC.R
@@ -17,7 +17,7 @@ CDK9_0m <- sort(corr_ctrl["CDK9",], decreasing = TRUE)
 NBC_0m <- sort(corr_ctrl["NBC",], decreasing = TRUE)
 NBC_speCTRL_0m <- sort(corr_ctrl["NBC_speCTRL",], decreasing = TRUE)
 NBC_common_0m <- sort(corr_ctrl["NBC_common",], decreasing = TRUE)
-NBC_speDEX_0m <- sort(corr_ctrl["NBC_speCTRL",], decreasing = TRUE)
+NBC_speDEX_0m <- sort(corr_ctrl["NBC_speDEX",], decreasing = TRUE)
 
 NIPBL_1h <- sort(corr_dex["NIPBL",], decreasing = TRUE)
 BRD4_1h <- sort(corr_dex["BRD4",], decreasing = TRUE)
@@ -25,15 +25,6 @@ CDK9_1h <- sort(corr_dex["CDK9",], decreasing = TRUE)
 NBC_1h <- sort(corr_dex["NBC",], decreasing = TRUE)
 NBC_speCTRL_1h <- sort(corr_dex["NBC_speCTRL",], decreasing = TRUE)
 NBC_common_1h <- sort(corr_dex["NBC_common",], decreasing = TRUE)
-NBC_speDEX_1h <- sort(corr_dex["NBC_speCTRL",], decreasing = TRUE)
+NBC_speDEX_1h <- sort(corr_dex["NBC_speDEX",], decreasing = TRUE)
 
-NIPBL_1h_df <- data.frame(cofactors = names(NIPBL_1h), overlaps = NIPBL_1h)
-heatmap(NIPBL_1h_df %>% as.matrix)
-
-g <- ggplot(NIPBL_1h_df) + geom_bar()
-
-
-
-plot_ly(df = NBC_common_1h, labels = rownames(NBC_common_1h), x = rownames(NBC_common_1h), y = NBC_common_1h$overlaps, type = "bar")
-plot_ly(x = colnames(corr_ctrl), y = rownames(corr_ctrl), z = corr_ctrl, type = "heatmap")
-heatmap(NIPBL_1h)
+# check #27 LEVELPLOT WITH LATTICE
