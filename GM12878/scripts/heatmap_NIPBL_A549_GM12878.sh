cec85027404d7e0523cb8497e62d857b49f66d74
chris11 <christophe.tav@gmail.com>
2018-12-11 15:48:43 -0500

    update

--- GM12878/scripts/heatmap_NIPBL_A549_GM12878.sh
+++ GM12878/scripts/heatmap_NIPBL_A549_GM12878.sh
@@ -8,8 +8,8 @@ PEAKS_DIR=output/chip-pipeline-GRCh38/peak_call/A549vsGM12878_NIPBL
 BW_DIR_A549=/home/chris/Bureau/sb_cofactor_hr/A549/output/chip-pipeline-GRCh38/tracks
 BW_DIR_GM12878=output/chip-pipeline-GRCh38/tracks
 
-MATRIX_NAME=20181121_A549vsGM12878_NIPBL_matrix.gzip
-HEATMAP_NAME=20181121_A549vsGM12878_NIPBL_heatmap_greens.png
+MATRIX_NAME=20181204_A549vsGM12878_NIPBL_matrix.gzip
+HEATMAP_NAME=20181204_A549vsGM12878_NIPBL_heatmap_greens.png
 
 ### computeMatrix
 computeMatrix reference-point --referencePoint center \
@@ -24,7 +24,6 @@ computeMatrix reference-point --referencePoint center \
 		$BW_DIR_A549/A549_CTRL_MED1_rep1.bw \
 		$BW_DIR_A549/A549_CTRL_SMC1A_rep1.bw \
 		$BW_DIR_GM12878/GM12878_NIPBL_rep1.bw \
-		$BW_DIR_GM12878/SRR1636861_BRD4.bw \
 		$BW_DIR_GM12878/GM12878_CDK9_rep1.bw \
 		$BW_DIR_GM12878/GM12878_MED1_rep1.bw \
 		$BW_DIR_GM12878/GM12878_SMC1_rep1.bw \
@@ -38,6 +37,6 @@ plotHeatmap \
 	--regionsLabel A549_NIPBL COMMON_NIPBL GM12878_NIPBL \
 	--samplesLabel \
 		A549_NIPBL A549_BRD4 A549_CDK9 A549_MED1 A549_SMC1A \
-		GM12878_NIPBL GM12878_BRD4 GM12878_CDK9 GM12878_MED1 GM12878_SMC1 \
-	--yMax 1000 \
+		GM12878_NIPBL GM12878_CDK9 GM12878_MED1 GM12878_SMC1 \
+	--whatToShow 'heatmap only'\
 	--outFileName $OUTPUT_DIR/$HEATMAP_NAME
