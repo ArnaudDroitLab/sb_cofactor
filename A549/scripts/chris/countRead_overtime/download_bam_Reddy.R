83e0e148e15429a5876a89c0e9dc85315d62302a
chris11 <christophe.tav@gmail.com>
2019-02-05 09:32:33 -0500

    update

--- A549/scripts/chris/countRead_overtime/download_bam_Reddy.R
+++ A549/scripts/chris/countRead_overtime/download_bam_Reddy.R
@@ -41,72 +41,32 @@ download_bam_in_AccessionList <- function(accession_list, report_bam) {
   }
 }
 
+download_Reddy_ChIP <- function(protein, WCE = FALSE) {
+  report_bam <- make_report_bam(target_name = protein, all_chip_bam)
+  if (WCE == TRUE) {
+    report_wce_bam <- make_report_WCE_bam(report_bam, all_chip_bam)
+    accession_list_wce <- unique(report_wce_bam$file_accession)
+    download_bam_in_AccessionList(accession_list_wce, report_wce_bam)
+  } else {
+    accession_list <- unique(report_bam$file_accession)
+    download_bam_in_AccessionList(accession_list, report_bam)
+  }
+}
+
 ### All bam files associated with ChIP-seq experiments in A549
 all_chip_bam <- ENCODExplorer::queryEncodeGeneric(biosample_name="A549", file_format = "bam", assay="ChIP-seq")
 
-### Download bam GR
-report_gr_bam <- make_report_bam(target_name = "NR3C1", all_chip_bam)
-accession_list_gr <- unique(report_gr_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_gr, report_gr_bam)
-
-### Download bam EP300
-report_ep300_bam <- make_report_bam(target_name = "EP300", all_chip_bam)
-accession_list_ep300 <- unique(report_ep300_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_ep300, report_ep300_bam)
-
-### Download bam WCE GR
-report_gr_wce_bam <- make_report_WCE_bam(report_gr_bam, all_chip_bam)
-accession_list_gr_wce <- unique(report_gr_wce_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_gr_wce, report_gr_wce_bam)
-
-### Download bam WCE GR
-report_ep300_wce_bam <- make_report_WCE_bam(report_ep300_bam, all_chip_bam)
-accession_list_ep300_wce <- unique(report_ep300_wce_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_ep300_wce, report_ep300_wce_bam)
-
-#######################
-# 2019-01-28
-#######################
-
-### Download bam CTCF
-report_ctcf_bam <- make_report_bam(target_name = "CTCF", all_chip_bam)
-accession_list_ctcf <- unique(report_ctcf_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_ctcf, report_ctcf_bam)
-
-### Download bam SMC3
-report_smc3_bam <- make_report_bam(target_name = "SMC3", all_chip_bam)
-accession_list_smc3 <- unique(report_smc3_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_smc3, report_smc3_bam)
-
-### Download bam RAD21
-report_rad21_bam <- make_report_bam(target_name = "RAD21", all_chip_bam)
-accession_list_rad21 <- unique(report_rad21_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_rad21, report_rad21_bam)
-
-### Download bam FOSL2
-report_fosl2_bam <- make_report_bam(target_name = "FOSL2", all_chip_bam)
-accession_list_fosl2 <- unique(report_fosl2_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_fosl2, report_fosl2_bam)
-
-#######################
-# 2019-01-29
-#######################
-
-### Download bam BCL3
-report_bcl3_bam <- make_report_bam(target_name = "BCL3", all_chip_bam)
-accession_list_bcl3 <- unique(report_bcl3_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_bcl3, report_bcl3_bam)
-
-### Download bam JUN
-report_jun_bam <- make_report_bam(target_name = "JUN", all_chip_bam)
-accession_list_jun <- unique(report_jun_bam$file_accession)
-# download_bam_in_AccessionList(accession_list_jun, report_jun_bam)
-
-# targets <- c("HES2", "CEBPB")
-targets <- c("CEBPB")
-for (target in targets) {
-  message("####### ", target)
-  report_bam <- make_report_bam(target_name = target, all_chip_bam)
-  accession_list <- unique(report_bam$file_accession)
-  download_bam_in_AccessionList(accession_list, report_bam)
-}
+### Download Bam
+download_Reddy_ChIP("NR3C1")
+download_Reddy_ChIP("EP300")
+download_Reddy_ChIP("NR3C1", WCE = TRUE)
+download_Reddy_ChIP("EP300", WCE = TRUE)
+download_Reddy_ChIP("CTCF")
+download_Reddy_ChIP("SMC3")
+download_Reddy_ChIP("RAD21")
+download_Reddy_ChIP("FOSL2")
+download_Reddy_ChIP("BCL3")
+download_Reddy_ChIP("JUN")
+download_Reddy_ChIP("JUNB")
+download_Reddy_ChIP("HES2")
+download_Reddy_ChIP("CEPBP")
