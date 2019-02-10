6729bda49cadd8642ef4f9b7ac9e9c6cd3ca7d83
chris11 <christophe.tav@gmail.com>
2019-02-10 15:16:11 -0500

    integrate load_reddy_binding_consensus function in load_reddy.R and merge the four countBam functions

--- A549/scripts/chris/countRead_overtime/countBam_Reddy.R
+++ A549/scripts/chris/countRead_overtime/countBam_Reddy.R
@@ -1,108 +1,56 @@
 setwd("/home/chris/Bureau/sb_cofactor_hr/A549")
 source("scripts/load_reddy.R")
-source("scripts/chris/countRead_overtime/load_Reddy_peaks_consensus.R")
 source("scripts/chris/countRead_overtime/countReads.utils.R")
 library(Rsamtools)
 library(knitr)
 library(dplyr)
 
 ######################
-countInBam <- function(target, indexBam = FALSE) {
+# background argument can be: FALSE, background or gaps_background
+countInBam <- function(target, indexBam = FALSE, background = FALSE) {
   ### List of bam
   report_bam <- make_report_bam(target_name = target, all_chip_bam)
-  bamPath <- generate_bamPath_from_report(report_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
-  if (indexBam == TRUE) {indexBam(bamPath)}
+  
+  if (background == "wce") {
+    report_wce_bam <- make_report_WCE_bam(report_bam, all_chip_bam)
+    bamPath_wce <- generate_bamPath_from_report(report_wce_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
+    if (indexBam == TRUE) {indexBam(bamPath_wce)}
+  } else {
+    bamPath <- generate_bamPath_from_report(report_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
+    if (indexBam == TRUE) {indexBam(bamPath)}
+  }
   
   ### Gather all peaks all over the time frame
   make_ENCODE_Reddy_ChIP_experiments_file(target)
   target_regions <- load_reddy_binding_consensus(target = target)
   all_regions_reduced <- get_reduced_peaks_from_list(target_regions)
-  summary(width(all_regions_reduced))
-  peaks_coordVector <- generate_coordVector(all_regions_reduced)
+  
+  if (background == "background") {
+    background_regions_reduced <- shift(all_regions_reduced, 10000)
+    peaks_background_coordVector <- generate_coordVector(background_regions_reduced)
+  } else if (background == "gaps_background") {
+    gapbackground_regions_reduced <- gaps(all_regions_reduced)
+    peaks_gapbackground_coordVector <- generate_coordVector(gapbackground_regions_reduced)
+  } else {
+    peaks_coordVector <- generate_coordVector(all_regions_reduced)
+  }
   
   ### Count reads
-  count_total <- countRead(all_regions_reduced, peaks_coordVector, bamPath, report_bam)
+  if (background == "wce") {
+    count_total <- countRead(all_regions_reduced, peaks_coordVector, bamPath_wce, report_bam_wce)
+  } else if (background == "background") {
+    count_total <- countRead(background_regions_reduced, peaks_background_coordVector, bamPath, report_bam)
+  } else if (background == "gaps_background") {
+    count_total <- countRead(gapbackground_regions_reduced, peaks_gapbackground_coordVector, bamPath, report_bam)
+  } else {
+    count_total <- countRead(all_regions_reduced, peaks_coordVector, bamPath, report_bam)
+  }
   
   ### Save
   filename <- paste0("count_total_", target)
-  filename_rdata <- paste0(filename, ".RData")
-  filename_txt <- paste0(filename, ".txt")
-  
-  save(count_total, file = file.path(output_path, filename_rdata))
-  write.table(count_total, file = file.path(output_path, filename_txt), sep = "\t", quote = FALSE, row.names = FALSE)
-}
-
-countInBam_wce <- function(target, indexBam = FALSE) {
-  ### List of bam
-  report_bam <- make_report_bam(target_name = target, all_chip_bam)
-  report_wce_bam <- make_report_WCE_bam(report_bam, all_chip_bam)
-  bamPath_wce <- generate_bamPath_from_report(report_wce_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
-  if (indexBam == TRUE) {indexBam(bamPath_wce)}
-  
-  ### Gather all peaks all over the time frame
-  make_ENCODE_Reddy_ChIP_experiments_file(target)
-  target_regions <- load_reddy_binding_consensus(target = target)
-  all_regions_reduced <- get_reduced_peaks_from_list(target_regions)
-  summary(width(all_regions_reduced))
-  peaks_coordVector <- generate_coordVector(all_regions_reduced)
-  
-  ### Count reads
-  count_total <- countRead(all_regions_reduced, peaks_coordVector, bamPath, report_bam)
-  
-  ### Save
-  filename <- paste0("count_total_", target, "_wce")
-  filename_rdata <- paste0(filename, ".RData")
-  filename_txt <- paste0(filename, ".txt")
-  
-  save(count_total, file = file.path(output_path, filename_rdata))
-  write.table(count_total, file = file.path(output_path, filename_txt), sep = "\t", quote = FALSE, row.names = FALSE)
-}
-
-countInBam_background <- function(target, indexBam = FALSE) {
-  report_bam <- make_report_bam(target_name = target, all_chip_bam)
-  bamPath <- generate_bamPath_from_report(report_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
-  if (indexBam == TRUE) {indexBam(bamPath)}
-  
-  ### Gather all peaks all over the time frame
-  make_ENCODE_Reddy_ChIP_experiments_file(target)
-  target_regions <- load_reddy_binding_consensus(target = target)
-  all_regions_reduced <- get_reduced_peaks_from_list(target_regions)
-  
-  background_regions_reduced <- shift(all_regions_reduced, 10000)
-  peaks_background_coordVector <- generate_coordVector(background_regions_reduced)
-  
-  ### Count reads
-  count_total <- countRead(background_regions_reduced, peaks_background_coordVector, bamPath, report_bam)
-  
-  ### Save
-  filename <- paste0("count_total_", target, "_background")
-  filename_rdata <- paste0(filename, ".RData")
-  filename_txt <- paste0(filename, ".txt")
-  
-  save(count_total, file = file.path(output_path, filename_rdata))
-  write.table(count_total, file = file.path(output_path, filename_txt), sep = "\t", quote = FALSE, row.names = FALSE)
-}
-
-countInBam_gapbackground <- function(target, indexBam = FALSE) {
-  report_bam <- make_report_bam(target_name = target, all_chip_bam)
-  bamPath <- generate_bamPath_from_report(report_bam, "/home/chris/Bureau/sb_cofactor_hr/A549/input/ENCODE/A549/GRCh38/chip-seq/bam/")
-  if (indexBam == TRUE) {indexBam(bamPath)}
-  
-  ### Gather all peaks all over the time frame
-  make_ENCODE_Reddy_ChIP_experiments_file(target)
-  target_regions <- load_reddy_binding_consensus(target = target)
-  all_regions_reduced <- get_reduced_peaks_from_list(target_regions)
-
-  gapbackground_regions_reduced <- gaps(all_regions_reduced)
-  summary(width(gapbackground_regions_reduced))
-  length(gapbackground_regions_reduced)
-  peaks_gapbackground_coordVector <- generate_coordVector(gapbackground_regions_reduced)
-  
-  ### Count reads
-  count_total <- countRead(gapbackground_regions_reduced, peaks_gapbackground_coordVector, bamPath, report_bam)
-  
-  ### Save
-  filename <- paste0("count_total_", target, "_gaps_background")
+  if (background != FALSE) {
+    filename <- paste0(filename, "_", background)
+  }
   filename_rdata <- paste0(filename, ".RData")
   filename_txt <- paste0(filename, ".txt")
   
@@ -115,18 +63,11 @@ all_chip_bam <- ENCODExplorer::queryEncodeGeneric(biosample_name="A549", file_fo
 output_path <- "/home/chris/Bureau/sb_cofactor_hr/A549/output/analyses/countTable_overtime"
 
 ######################
-target <- c("NR3C1", "EP300")
-for (protein in target) {
+targets <- c("NR3C1", "EP300", "SMC3", "CTCF", "RAD21", "FOSL2", "BCL3", "JUN", "JUNB", "HES2", "CEBPB")
+for (protein in targets) {
   message("#####\t", protein)
-  countInBam(protein, indexBam = TRUE)
-  countInBam_gapbackground(protein, indexBam = FALSE)
-}
-
-targets2 <- c("SMC3", "CTCF", "RAD21", "FOSL2", "BCL3", "JUN", "JUNB", "HES2", "CEBPB")
-for (protein in targets2) {
-  message("#####\t", protein)
-  countInBam(protein, indexBam = TRUE)
-  countInBam_wce(protein, indexBam = TRUE)
-  countInBam_background(protein, indexBam = FALSE)
-  countInBam_gapbackground(protein, indexBam = FALSE)
+  # countInBam(protein, indexBam = FALSE)
+  # countInBam(protein, indexBam = FALSE, background = "wce")
+  # countInBam(protein, indexBam = FALSE, background = "background")
+  # countInBam(protein, indexBam = FALSE, background = "gaps_background")
 }
