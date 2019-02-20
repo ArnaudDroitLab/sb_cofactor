d60a6719ad3bbd7081d5ac0d4efed340c793645c
chris11 <christophe.tav@gmail.com>
2019-02-20 16:51:55 -0500

    script to generate pipeline to process HEPG2 data

---
+++ HEPG2/scripts/generate_pipeline.sh
@@ -0,0 +1,12 @@
+mkdir -p output/chip-pipeline-GRCh38
+
+chipseq.py -j slurm -s '1-12' \
+    -l debug \
+    -r raw/chip-seq/readset.txt \
+    -d raw/chip-seq/design.txt \
+    -o output/chip-pipeline-GRCh38 \
+    --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
+        $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.cedar.ini \
+        $MUGQIC_INSTALL_HOME/genomes/species/Homo_sapiens.GRCh38/Homo_sapiens.GRCh38.ini \
+        input/cedar_assembly_bugfix.txt
+
