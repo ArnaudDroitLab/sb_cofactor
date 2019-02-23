34c245a3a6a870e23a5cd0fefa789f8cde3009c9
chriis11 <christophe.tav@gmail.com>
2019-02-23 13:14:34 -0500

    first attempt to download fastq file directly on cedar

--- MCF7/scripts/download_BRD4_from_GSE55923.sh
+++ MCF7/scripts/download_BRD4_from_GSE55923.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-export RAP_ID=def-stbil30
-export JOB_MAIL="christophe.tav@gmail.com"
 module load sra-toolkit/2.8.2-1
 
 export OUTDIR="/home/chris11/scratch/sb_cofactor/MCF7/raw/chip-seq"
+
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=def-stbil30
 
 for sra in SRX490426S SRX490427 SRX490428 SRX490429 SRX490430 SRX490431
 do
