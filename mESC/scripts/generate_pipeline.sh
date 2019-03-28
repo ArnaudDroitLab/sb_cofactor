b520b03f7696fb76163de93be0c787f5ef292b5e
chriis11 <christophe.tav@gmail.com>
2019-03-28 10:18:37 -0400

    update

--- mESC/scripts/generate_pipeline.sh
+++ mESC/scripts/generate_pipeline.sh
@@ -1,12 +1,11 @@
-mkdir -p $SCRATCH/sb_cofactor/HEPG2/output/chip-pipeline-GRCh38
+mkdir -p output/chip-pipeline-GRCm38
 
 chipseq.py -j slurm -s '1-10,12' \
     -l debug \
-    -r $SCRATCH/sb_cofactor/HEPG2/raw/chip-seq/readset.txt \
-    -d $SCRATCH/sb_cofactor/HEPG2/raw/chip-seq/design.txt \
-    -o $SCRATCH/sb_cofactor/HEPG2/output/chip-pipeline-GRCh38 \
+    -r raw/chip-seq/readset.txt \
+    -d raw/chip-seq/design.txt \
+    -o output/chip-pipeline-GRCm38 \
     --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
         $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.cedar.ini \
-        $MUGQIC_INSTALL_HOME/genomes/species/Homo_sapiens.GRCh38/Homo_sapiens.GRCh38.ini \
+        $MUGQIC_INSTALL_HOME/genomes/species/Mus_musculus.GRCm38/Mus_musculus.GRCm38.ini \
         input/cedar_assembly_bugfix.txt
-
