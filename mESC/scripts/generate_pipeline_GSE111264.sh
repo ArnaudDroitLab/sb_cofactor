98d5689e6253dbbbd65d3b66c840a8d3a7cda03a
chriis11 <christophe.tav@gmail.com>
2019-03-28 11:18:12 -0400

    use GRCm38 as reference genome

--- mESC/scripts/generate_pipeline_GSE111264.sh
+++ mESC/scripts/generate_pipeline_GSE111264.sh
@@ -1,9 +1,9 @@
-mkdir -p output/chip-pipeline-GRCm38
+mkdir -p output/chip-pipeline-GRCm38_GSE111264
 
 chipseq.py -j slurm -s '1-10,12' \
     -l debug \
-    -r raw/chip-seq/readset.txt \
-    -d raw/chip-seq/design.txt \
+    -r raw/chip-seq/readset_GSE111264.txt \
+    -d raw/chip-seq/design_GSE111264.txt \
     -o output/chip-pipeline-GRCm38 \
     --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
         $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.cedar.ini \
