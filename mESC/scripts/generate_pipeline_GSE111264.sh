dc52e77e83d777bd2595f4cccdaa8abe582ab76c
chriis11 <christophe.tav@gmail.com>
2019-03-28 16:16:39 -0400

    update GSE111264 pipeline

--- mESC/scripts/generate_pipeline_GSE111264.sh
+++ mESC/scripts/generate_pipeline_GSE111264.sh
@@ -4,7 +4,7 @@ chipseq.py -j slurm -s '1-10,12' \
     -l debug \
     -r raw/chip-seq/readset_GSE111264.txt \
     -d raw/chip-seq/design_GSE111264.txt \
-    -o output/chip-pipeline-GRCm38 \
+    -o output/chip-pipeline-GRCm38_GSE111264 \
     --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
         $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.cedar.ini \
         $MUGQIC_INSTALL_HOME/genomes/species/Mus_musculus.GRCm38/Mus_musculus.GRCm38.ini \
