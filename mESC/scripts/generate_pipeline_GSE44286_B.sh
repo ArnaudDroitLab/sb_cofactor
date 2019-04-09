5be697b8ad3c3b588ed6cc50ddbff183e8ab9194
chriis11 <christophe.tav@gmail.com>
2019-04-09 10:53:57 -0400

    trimmomatic parameters

---
+++ mESC/scripts/generate_pipeline_GSE44286_B.sh
@@ -0,0 +1,12 @@
+mkdir -p output/chip-pipeline-GRCm38_GSE44286_B
+
+chipseq.py -j slurm -s '1-10,12' \
+    -l debug \
+    -r raw/chip-seq/readset_GSE44286_B.txt \
+    -d raw/chip-seq/design_GSE44286_B.txt \
+    -o output/chip-pipeline-GRCm38_GSE44286_B \
+    --config $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.base.ini \
+        $MUGQIC_PIPELINES_HOME/pipelines/chipseq/chipseq.cedar.ini \
+        $MUGQIC_INSTALL_HOME/genomes/species/Mus_musculus.GRCm38/Mus_musculus.GRCm38.ini \
+        input/cedar_assembly_bugfix.txt \
+        input/cedar_T??M??.txt
