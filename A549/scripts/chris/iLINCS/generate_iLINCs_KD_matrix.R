e1e27d8d4a7eb2f6a4bddaea204975d0291ef674
chriis11 <christophe.tav@gmail.com>
2019-04-17 17:47:46 -0400

    update

--- A549/scripts/chris/iLINCS/generate_iLINCs_KD_matrix.R
+++ A549/scripts/chris/iLINCS/generate_iLINCs_KD_matrix.R
@@ -23,36 +23,45 @@ SMARCB1 <- read.csv("input/iLINCS_KD/SMARCB1_sig_Sat_Apr_13_19_42_20_2019_424526
 SMARCE1 <- read.csv("input/iLINCS_KD/SMARCE1_sig_Sat_Apr_13_19_42_43_2019_8850870.xls", sep="\t") %>% select(Value_LogDiffExp)
 SMC3 <- read.csv("input/iLINCS_KD/SMC3_sig_Sat_Apr_13_19_21_41_2019_1818884.xls", sep="\t") %>% select(Value_LogDiffExp)
 
-CTCF <- read.csv("input/iLINCS_KD/CTCF_sig_Sat_Apr_13_19_54_22_2019_8607779.xls", sep="\t") %>% select(Value_LogDiffExp)
-POLR2A <- read.csv("input/iLINCS_KD/POLR2A_sig_Sat_Apr_13_19_56_41_2019_7748946.xls", sep="\t") %>% select(Value_LogDiffExp)
-CHEK1 <- read.csv("input/iLINCS_KD/CHEK1_sig_Sun_Apr_14_20_24_16_2019_4192854.xls", sep="\t") %>% select(Value_LogDiffExp)
-CCL2 <- read.csv("input/iLINCS_KD/CCL2_sig_Sun_Apr_14_20_24_11_2019_2966412.xls", sep="\t") %>% select(Value_LogDiffExp)
-B2M <- read.csv("input/iLINCS_KD/B2M_sig_Sun_Apr_14_20_23_38_2019_8911239.xls", sep="\t") %>% select(Value_LogDiffExp)
-ADAR <- read.csv("input/iLINCS_KD/ADAR_sig_Sun_Apr_14_20_22_51_2019_8870966.xls", sep="\t") %>% select(Value_LogDiffExp)
-ABCA1 <- read.csv("input/iLINCS_KD/ABCA1_sig_Sun_Apr_14_20_21_56_2019_6692401.xls", sep="\t") %>% select(Value_LogDiffExp)
-ABAT <- read.csv("input/iLINCS_KD/ABAT_sig_Sun_Apr_14_20_21_51_2019_9531899.xls", sep="\t") %>% select(Value_LogDiffExp)
-AATF <- read.csv("input/iLINCS_KD/AATF_sig_Sun_Apr_14_20_21_48_2019_8154118.xls", sep="\t") %>% select(Value_LogDiffExp)
-AARS <- read.csv("input/iLINCS_KD/AARS_sig_Sun_Apr_14_20_21_12_2019_3064136.xls", sep="\t") %>% select(Value_LogDiffExp)
-A2M <- read.csv("input/iLINCS_KD/A2M_sig_Sun_Apr_14_20_20_19_2019_1039643.xls", sep="\t") %>% select(Value_LogDiffExp)
+# CTCF <- read.csv("input/iLINCS_KD/CTCF_sig_Sat_Apr_13_19_54_22_2019_8607779.xls", sep="\t") %>% select(Value_LogDiffExp)
+# POLR2A <- read.csv("input/iLINCS_KD/POLR2A_sig_Sat_Apr_13_19_56_41_2019_7748946.xls", sep="\t") %>% select(Value_LogDiffExp)
+# CHEK1 <- read.csv("input/iLINCS_KD/CHEK1_sig_Sun_Apr_14_20_24_16_2019_4192854.xls", sep="\t") %>% select(Value_LogDiffExp)
+# CCL2 <- read.csv("input/iLINCS_KD/CCL2_sig_Sun_Apr_14_20_24_11_2019_2966412.xls", sep="\t") %>% select(Value_LogDiffExp)
+# B2M <- read.csv("input/iLINCS_KD/B2M_sig_Sun_Apr_14_20_23_38_2019_8911239.xls", sep="\t") %>% select(Value_LogDiffExp)
+# ADAR <- read.csv("input/iLINCS_KD/ADAR_sig_Sun_Apr_14_20_22_51_2019_8870966.xls", sep="\t") %>% select(Value_LogDiffExp)
+# ABCA1 <- read.csv("input/iLINCS_KD/ABCA1_sig_Sun_Apr_14_20_21_56_2019_6692401.xls", sep="\t") %>% select(Value_LogDiffExp)
+# ABAT <- read.csv("input/iLINCS_KD/ABAT_sig_Sun_Apr_14_20_21_51_2019_9531899.xls", sep="\t") %>% select(Value_LogDiffExp)
+# AATF <- read.csv("input/iLINCS_KD/AATF_sig_Sun_Apr_14_20_21_48_2019_8154118.xls", sep="\t") %>% select(Value_LogDiffExp)
+# AARS <- read.csv("input/iLINCS_KD/AARS_sig_Sun_Apr_14_20_21_12_2019_3064136.xls", sep="\t") %>% select(Value_LogDiffExp)
+# A2M <- read.csv("input/iLINCS_KD/A2M_sig_Sun_Apr_14_20_20_19_2019_1039643.xls", sep="\t") %>% select(Value_LogDiffExp)
+
+# df <- data.frame(GeneSymbol, AFF4, BRPF1, CREBBP, EP300, HDAC4,
+#                  HDAC8, KAT6A, KAT6B, NIPBL, SMARCA2, 
+#                  SMARCA4, SMARCB1, SMARCE1, SMC3, CTCF, POLR2A,
+#                  CHEK1, CCL2, B2M, ADAR, ABCA1, ABAT, AATF, AARS, A2M)
 
 df <- data.frame(GeneSymbol, AFF4, BRPF1, CREBBP, EP300, HDAC4,
-                 HDAC8, KAT6A, KAT6B, NIPBL, SMARCA2, 
-                 SMARCA4, SMARCB1, SMARCE1, SMC3, CTCF, POLR2A,
-                 CHEK1, CCL2, B2M, ADAR, ABCA1, ABAT, AATF, AARS, A2M)
+                 HDAC8, KAT6A, KAT6B, NIPBL, SMARCA2,
+                 SMARCA4, SMARCB1, SMARCE1, SMC3)
+
+# colnames(df) <- c("Gene", "AFF4", "BRPF1", "CREBBP", "EP300", "HDAC4",
+#                   "HDAC8", "KAT6A", "KAT6B", "NIPBL", "SMARCA2", 
+#                   "SMARCA4", "SMARCB1", "SMARCE1", "SMC3", "CTCF", "POLR2A",
+#                   "CHEK1", "CCL2", "B2M", "ADAR", "ABCA1", "ABAT", "AATF", "AARS", "A2M")
 
 colnames(df) <- c("Gene", "AFF4", "BRPF1", "CREBBP", "EP300", "HDAC4",
                   "HDAC8", "KAT6A", "KAT6B", "NIPBL", "SMARCA2", 
-                  "SMARCA4", "SMARCB1", "SMARCE1", "SMC3", "CTCF", "POLR2A",
-                  "CHEK1", "CCL2", "B2M", "ADAR", "ABCA1", "ABAT", "AATF", "AARS", "A2M")
+                  "SMARCA4", "SMARCB1", "SMARCE1", "SMC3")
 
 rownames(df) <- GeneSymbol
 
 mat <- as.matrix(df[,2:ncol(df)])
 
-Heatmap(mat,
+Heatmap(mat, name = "LogDiffExp",
         row_names_side = "left",
         row_names_gp = gpar(fontsize = 0),
         row_dend_side = "right",
+        show_row_dend = FALSE,
         column_names_side = "top",
         column_names_gp = gpar(fontsize = 11),
         column_names_rot = 45,
