a5b61444b943ec5442686fef5684ec8fa2d488f2
chriis11 <christophe.tav@gmail.com>
2019-04-25 15:08:53 -0400

    hclust

---
+++ mESC/scripts/ilincs/LINCS_KD_A549_clustering.R
@@ -0,0 +1,67 @@
+setwd("/home/chris/Bureau/sb_cofactor_hr/mESC")
+
+library(dplyr)
+library(factoextra)
+library(ComplexHeatmap)
+library(circlize)
+
+col_fun = colorRamp2(c(-2, 0, 2), c("#0f4259", "white", "#800020"))
+
+mutated_cofactors <- c("NIPBL", "SMC1A", "SMC3", "RAD21", "HDAC8", "BRD4", "CREBBP", "EP300", "KAT6A", "KAT6B",
+                       "SRCAP", "BRPF1", "RAI1", "MBD5", "EHMT1", "HDAC4", "ARID1A", "ARID1B", "SMARCA4", "SMARCB1",
+                       "SMARCE1", "ANKRD11", "KMT2A", "SMARCA2", "AFF4", "KMT2D", "KDM6A", "TAF6", "ESCO2", "CHD7",
+                       "DDX11", "STAG1", "STAG2", "SETD5", "MED12", "MED13L", "MED17", "MED23", "MED25")
+
+A549_ILINCs_KD_matrix <- readRDS("output/analysis/ILINCS/A549_ILINCs_KD_matrix.rds")
+
+rownames(A549_ILINCs_KD_matrix) <- A549_ILINCs_KD_matrix$Name_GeneSymbol
+A549_ILINCs_KD_matrix <- A549_ILINCs_KD_matrix %>% select(-Name_GeneSymbol)
+
+protKD <- colnames(A549_ILINCs_KD_matrix)
+mutated <- ifelse(protKD %in% mutated_cofactors, TRUE, FALSE)
+isMutated <- data.frame(protKD, mutated)
+
+# Heatmap
+mat <- as.matrix(A549_ILINCs_KD_matrix)
+max(mat)
+min(mat)
+
+Heatmap(mat, name = "LogDiffExp",
+        row_names_side = "left",
+        row_names_gp = gpar(fontsize = 0),
+        row_dend_side = "right",
+        show_row_dend = FALSE,
+        show_column_names = FALSE,
+        column_names_side = "top",
+        column_names_gp = gpar(fontsize = 11),
+        column_names_rot = 45,
+        column_dend_side = "bottom",
+        show_column_dend = FALSE,
+        row_dend_width = unit(50, "mm"),
+        column_dend_height = unit(50, "mm"),
+        col = col_fun,
+        rect_gp = gpar(col = "white", lwd = 0))
+        
+#
+res.dist <- get_dist(t(A549_ILINCs_KD_matrix), stand = TRUE, method = "pearson")
+fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+
+#
+dmat <- dist(t(mat))
+clusters <- hclust(dmat)
+plot(clusters)
+
+clusterCut3 <- cutree(clusters, 3)
+table(clusterCut3, isMutated$mutated)
+
+clusterCut4 <- cutree(clusters, 4)
+table(clusterCut4, isMutated$mutated)
+
+clusterCut10 <- cutree(clusters, 10)
+table(clusterCut10, isMutated$mutated)
+
+clusterCut20 <- cutree(clusters, 20)
+table(clusterCut20, isMutated$mutated)
+
+clusterCut30 <- cutree(clusters, 30)
+table(clusterCut30, isMutated$mutated)
