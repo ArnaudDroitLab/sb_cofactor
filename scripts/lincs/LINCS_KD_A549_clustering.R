098c2dc6dfc5ea64e1cafcf6ae398cdf5e78a400
Christophe Tav <chris@macbook-chris.local>
2019-05-06 13:50:50 -0400

    update

--- scripts/lincs/LINCS_KD_A549_clustering.R
+++ scripts/lincs/LINCS_KD_A549_clustering.R
@@ -1,28 +1,30 @@
-# setwd("/home/chris/Bureau/sb_cofactor_hr/mESC")
-setwd("/Users/chris/Desktop/sb_cofactor_hr/mESC")
+# setwd("/home/chris/Bureau/sb_cofactor_hr")
+setwd("/Users/chris/Desktop/sb_cofactor_hr")
 
-library(dplyr)
 library(factoextra)
 library(ComplexHeatmap)
 library(circlize)
-source("scripts/ilincs/lincs.utils.R")
+source("scripts/lincs/lincs.utils.R")
 
+###
 col_fun = colorRamp2(c(-2, 0, 2), c("#0f4259", "white", "#800020"))
 
-A549_ILINCs_KD_matrix <- readRDS("output/analysis/ILINCS/A549_ILINCs_KD_matrix.rds")
+A549_LINCs_KD_matrix <- readRDS("output/analysis/lincs/A549_LINCs_KD_matrix.rds")
+MUTATED_COFACTORS <- get_mutated_cofactors()
 
-rownames(A549_ILINCs_KD_matrix) <- A549_ILINCs_KD_matrix$Name_GeneSymbol
-A549_ILINCs_KD_matrix <- A549_ILINCs_KD_matrix %>% select(-Name_GeneSymbol)
+rownames(A549_LINCs_KD_matrix) <- A549_LINCs_KD_matrix$Name_GeneSymbol
+A549_LINCs_KD_matrix <- A549_LINCs_KD_matrix %>% select(-Name_GeneSymbol)
 
-protKD <- colnames(A549_ILINCs_KD_matrix)
-mutated <- ifelse(protKD %in% mutated_cofactors, TRUE, FALSE)
+protKD <- colnames(A549_LINCs_KD_matrix)
+mutated <- ifelse(protKD %in% MUTATED_COFACTORS, TRUE, FALSE)
 isMutated <- data.frame(protKD, mutated)
 
-mat <- as.matrix(A549_ILINCs_KD_matrix)
+mat <- as.matrix(A549_LINCs_KD_matrix)
 max(mat)
 min(mat)
 
 # Heatmap
+# column_names_rot does not exist anymore
 Heatmap(mat, name = "LogDiffExp",
         row_names_side = "left",
         row_names_gp = gpar(fontsize = 0),
@@ -31,7 +33,7 @@ Heatmap(mat, name = "LogDiffExp",
         show_column_names = FALSE,
         column_names_side = "top",
         column_names_gp = gpar(fontsize = 11),
-        column_names_rot = 45,
+        # column_names_rot = 45,
         column_dend_side = "bottom",
         show_column_dend = FALSE,
         row_dend_width = unit(50, "mm"),
@@ -40,12 +42,13 @@ Heatmap(mat, name = "LogDiffExp",
         rect_gp = gpar(col = "white", lwd = 0))
         
 # test method from package factoextra to plot distance matrix
-# res.dist <- get_dist(t(A549_ILINCs_KD_matrix), stand = TRUE, method = "pearson")
+# res.dist <- get_dist(t(A549_LINCs_KD_matrix), stand = TRUE, method = "pearson")
 # fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
 
 #
 dmat <- dist(t(mat))
 clusters <- hclust(dmat)
+
 plot(clusters)
 
 clusterCut3 <- cutree(clusters, 3)
--- scripts/lincs/available_HSF1.R
+++ scripts/lincs/available_HSF1.R
@@ -4,7 +4,7 @@ setwd("/Users/chris/Desktop/sb_cofactor_hr")
 library(knitr)
 source("scripts/lincs/lincs.utils.R")
 
-all_signatures <- read.csv("input/LINCS/LINCS_consensus_gene_KD_Signatures_all_37275.xls", sep = "\t")
+all_signatures <- read.csv("input/lincs/LINCS_consensus_gene_KD_Signatures_all_37275.xls", sep = "\t")
 
 hsf1 <- all_signatures %>% filter(TargetGene == "HSF1")
 
