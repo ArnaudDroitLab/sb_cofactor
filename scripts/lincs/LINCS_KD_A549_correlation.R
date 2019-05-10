8bbde17919dde0e2d1e7c8a26e9a54e7f6ee111a
Christophe Tav <chris@macbook-chris.local>
2019-05-10 11:52:50 -0400

    generate heatmap for close KD

--- scripts/lincs/LINCS_KD_A549_correlation.R
+++ scripts/lincs/LINCS_KD_A549_correlation.R
@@ -5,14 +5,8 @@ library(ComplexHeatmap)
 library(circlize)
 source("scripts/lincs/lincs.utils.R")
 
-### Define colors for heatmap
-col_fun = colorRamp2(c(-2, 0, 2), c("#0f4259", "white", "#800020"))
-
 # Load fold change gene expression values
 A549_LINCs_KD_matrix <- readRDS("output/analysis/lincs/A549_LINCs_KD_matrix.rds")
-
-# Load mutated cofactors names
-MUTATED_COFACTORS <- get_mutated_cofactors()
 
 # Remove gene name in order to build a matrix
 rownames(A549_LINCs_KD_matrix) <- A549_LINCs_KD_matrix$Name_GeneSymbol
@@ -23,23 +17,62 @@ mat <- as.matrix(A549_LINCs_KD_matrix)
 max(mat)
 min(mat)
 
+# Load mutated cofactors names
+MUTATED_COFACTORS <- get_mutated_cofactors()
+# MUTCOF_A549 is the list of available KD of mutated cofactors in A549 LINCS
+MUTCOF_A549 <- MUTATED_COFACTORS[MUTATED_COFACTORS %in% colnames(A549_LINCs_KD_matrix)]
+
+#######
 # Pearson correlation matrix between each KD sample
 cor.pearson <- cor(mat, method = "pearson")
 max(cor.pearson - diag(nrow(cor.pearson)))
 min(cor.pearson)
 
-# Which KD protein are close to the one I'm interested
-closeToNIPBL <- sort(cor.pearson["NIPBL", ], decreasing = TRUE)[1:20]
-matNIPBL <- as.matrix(A549_LINCs_KD_matrix[, names(closeToNIPBL)])
-Heatmap(matNIPBL, name = "LogDiffExp",
-        top_annotation = HeatmapAnnotation(correlation = anno_text(round(closeToNIPBL,2))),
-        show_row_names = FALSE,
-        show_row_dend = FALSE,
-        column_names_side = "top",
-        cluster_columns = FALSE,
-        col = col_fun)
+#######
+# Which KD are the closer from my mutated cofactors in term of gene signature?
+col_fun = colorRamp2(c(-2, 0, 2), c("#0f4259", "white", "#800020")) # Define colors for heatmap
+output <- "output/analysis/lincs/closeToMutatedCofactors"
+dir.create(output)
+date <- "20190510"
 
-for (cof in MUTATED_COFACTORS) {
+for (COF in MUTCOF_A549) {
+  closeToCOF <- sort(cor.pearson[COF, ], decreasing = TRUE)[1:20]
+  matCOF <-  as.matrix(A549_LINCs_KD_matrix[, names(closeToCOF)])
   
+  pdf_name <- file.path(output, paste0(COF, "_top20closerKD_", date, ".pdf"))
+  pdf(pdf_name)
+  heatmapCOF <- Heatmap(matCOF, name = "LogDiffExp",
+          show_row_names = FALSE,
+          show_row_dend = FALSE,
+          column_names_side = "top",
+          column_names_rot = 45,
+          cluster_columns = FALSE,
+          col = col_fun,
+          bottom_annotation = HeatmapAnnotation(correlation = anno_text(round(closeToCOF,3),
+                                                                        rot = 0,
+                                                                        just = "center")))
+  heatmapCOF
+  dev.off()
 }
 
+#######
+# Pearson correlation matrix between each KD sample
+mat_mutcof <- mat[, MUTCOF_A549]
+cor.pearson.mutcof <- cor.pearson[MUTCOF_A549, MUTCOF_A549]
+max(cor.pearson.mutcof - diag(nrow(cor.pearson.mutcof)))
+min(cor.pearson.mutcof)
+
+col_fun1 = colorRamp2(c(-1, 0, 1), c("#0f4259", "white", "#800020"))
+Heatmap(cor.pearson.mutcof, name = "Pearson's correlation",
+        row_names_side = "left",
+        row_dend_side = "right",
+        column_names_side = "top",
+        column_dend_side = "bottom",
+        row_dend_width = unit(30, "mm"),
+        column_dend_height = unit(30, "mm"),
+        column_dend_reorder = TRUE,
+        col = col_fun1,
+        rect_gp = gpar(col = "white", lwd = 1),
+        cell_fun = function(j, i, x, y, width, height, fill) {
+        grid.text(sprintf("%.2f", cor.pearson.mutcof[i, j]), x, y, gp = gpar(fontsize = 10))
+        })
