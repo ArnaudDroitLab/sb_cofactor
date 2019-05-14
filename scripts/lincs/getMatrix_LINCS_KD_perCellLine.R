fa5395232860f4059a38c8c8c02e896df44cc384
Christophe Tav <chris@macbook-chris.local>
2019-05-14 12:08:03 -0400

    keep all cell lines available at 96h and where KD of mutated cofactors have been done

--- scripts/lincs/getMatrix_LINCS_KD_perCellLine.R
+++ scripts/lincs/getMatrix_LINCS_KD_perCellLine.R
@@ -18,15 +18,12 @@ overview_KD <- all_signatures %>% group_by(CellLine)
 overview_KD_allTime <- overview_KD %>% summarize(NumberOfKD = n(), MutatedCofactor = sum(TargetGene %in% MUTATED_COFACTORS))
 kable(overview_KD_allTime)
 
-# Only 96h: time point available in all our favorites cell lines
+# Only 96h
 overview_KD_96h <- overview_KD %>% filter(Time == "96 h") %>% summarize(NumberOfKD = n(), MutatedCofactor = sum(TargetGene %in% MUTATED_COFACTORS))
 kable(overview_KD_96h)
 kable(overview_KD_96h %>% filter(MutatedCofactor != 0))
-
-# Our cell lines at 96 h
-cell_lines <- c("A375", "A549", "ASC", "HA1E", "HCC515", "HEKTE", "HEPG2", "HT29", "MCF7", "NPC", "PC3")
-overview_KD_96h_OurCellLines <- overview_KD_96h %>% filter(CellLine %in% cell_lines)
-kable(overview_KD_96h_OurCellLines)
+our_cell_lines <- overview_KD_96h %>% filter(MutatedCofactor != 0) %>% pull(CellLine)
+our_cell_lines
 
 #### Downloading matrix
 cell_lines_toDL <- c("HEKTE")
