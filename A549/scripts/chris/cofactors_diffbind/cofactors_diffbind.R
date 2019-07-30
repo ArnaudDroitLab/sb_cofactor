6904d41fa7dfbd1643a87676aae6dddb30ffbadf
chris <christophe.tav@gmail.com>
2019-07-30 16:32:08 -0400

    add function to display UpSet plot

--- A549/scripts/chris/cofactors_diffbind/cofactors_diffbind.R
+++ A549/scripts/chris/cofactors_diffbind/cofactors_diffbind.R
@@ -3,6 +3,33 @@
 library(ComplexHeatmap)
 source("scripts/ckn_utils.R")
 
+#####
+displayUpSet <- function(combMat, threshold = 1, customSetOrder = FALSE) {
+  combMat <- combMat[comb_size(combMat) >= threshold]
+  annot_top <- HeatmapAnnotation("Intersection\nsize" = anno_barplot(comb_size(combMat), 
+                                                                     border = FALSE,
+                                                                     gp = gpar(fill = "black"),
+                                                                     height = unit(3, "cm")), 
+                                 "Size" = anno_text(comb_size(combMat),
+                                                    rot = 0,
+                                                    just = "center",
+                                                    location = 0.25),
+                                 annotation_name_side = "left", annotation_name_rot = 0)
+  annot_right <- rowAnnotation("Set size" = anno_barplot(set_size(combMat), 
+                                                         border = FALSE, 
+                                                         gp = gpar(fill = "black"), 
+                                                         width = unit(2, "cm")),
+                               "Size" = anno_text(set_size(combMat)))
+  
+  if (customSetOrder != FALSE) {
+    UpSet(combMat, top_annotation = annot_top, right_annotation = annot_right,
+          set_order = customSetOrder)
+  } else {
+    UpSet(combMat, top_annotation = annot_top, right_annotation = annot_right)
+  }
+}
+
+#####
 cofactors <- load_diffbind_cofactors_peaks()
 cofactors <- cofactors[grep("NIPBL|BRD4", names(cofactors))]
 sapply(cofactors, length)
@@ -17,21 +44,5 @@ colnames(matrix_cofactors)
 cofactors_names <- c("BRD4", "NIPBL")
 cofactors_set_order <- c(paste0(cofactors_names, "_UP"), paste0(cofactors_names, "_UNBIASED"), paste0(cofactors_names, "_DOWN"))
 m4 <- make_comb_mat(matrix_cofactors, remove_empty_comb_set = TRUE)
-m4 <- m4[comb_size(m4) >= 10]
-UpSet(m4, set_order = cofactors_set_order)
-comb_size(m4)
 
-annot_top <- HeatmapAnnotation("Intersection\nsize" = anno_barplot(comb_size(m4), 
-                                                                   border = FALSE, gp = gpar(fill = "black"), height = unit(3, "cm")), 
-                               annotation_name_side = "left", annotation_name_rot = 0,
-                               "Size" = anno_text(comb_size(m4), rot = 0, just = "center", location = 0.25))
-annot_right <- rowAnnotation("Set size" = anno_barplot(set_size(m4), 
-                                                       border = FALSE, 
-                                                       gp = gpar(fill = "black"), 
-                                                       width = unit(2, "cm")),
-                             "Size" = anno_text(set_size(m4))
-)
-
-cofactors_names <- c("BRD4", "NIPBL")
-UpSet(m4, top_annotation = annot_top, right_annotation = annot_right,
-      set_order = cofactors_set_order)
+combMat <- displayUpSet(combMat = m4, threshold = 30)
