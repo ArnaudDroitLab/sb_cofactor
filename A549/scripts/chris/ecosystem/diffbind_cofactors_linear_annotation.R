1f1f78aad91e65faacfdf8690b64c78d37b88a87
Christophe Tav <christophe.tav@gmail.com>
2019-08-27 19:19:24 -0400

    hcr brd4

--- A549/scripts/chris/ecosystem/diffbind_cofactors_linear_annotation.R
+++ A549/scripts/chris/ecosystem/diffbind_cofactors_linear_annotation.R
@@ -82,7 +82,7 @@ sapply(genes_reg_by_cofactors_viaLinear, length)
 # saveRDS(genes_reg_by_cofactors_viaLinear, file = "output/analyses/ecosystem/genes_reg_by_cofactors_viaLinear.rds")
 
 # HCR
-hcr <- GenomicRanges::reduce(gr_regions, min.gapwidth = 25000)
+hcr <- GenomicRanges::reduce(gr_regions, min.gapwidth = 25000) # 15102
 summary(width(hcr))
 hcr <- hcr[width(hcr) > 0]
 summary(width(hcr))
@@ -92,46 +92,8 @@ stdchr_promoters_A549 <- keepStdChr(raw_promoters_A549)
 stdchr_promoters_A549$tx_id <- names(stdchr_promoters_A549)
 names(stdchr_promoters_A549) <- stdchr_promoters_A549$gene_id
 
-# hcr_vs_genes <- list()
-# for (cofactor in cofactors) {
-#   geneup <- genes_reg_by_cofactors_viaLinear[[paste(cofactor, "UP", sep = "_")]]
-#   tss_up <- stdchr_promoters_A549[geneup]
-#   tss_up$direction <- "up"
-#   genedown <- genes_reg_by_cofactors_viaLinear[[paste(cofactor, "DOWN", sep = "_")]]
-#   tss_down <- stdchr_promoters_A549[genedown]
-#   tss_down$direction <- "down"
-#   
-#   hcr_cofactor <- hcr_vs_genes[[cofactor]]
-#   
-#   for (i in 1:length(hcr)) {
-#     hcr_name <- paste("HCR", i, sep = "_")
-#     hcr_x <- hcr[i]
-#     
-#     ov <- GRanges()
-#     ov <- c(ov,
-#             subsetByOverlaps(tss_up, hcr_x),
-#             subsetByOverlaps(tss_down, hcr_x))
-#     
-#     if (length(ov) > 1) {
-#       hcr_vs_genes[[cofactor]][[hcr_name]] <- as.data.frame(ov)
-#       message("##### ", cofactor, " | ", hcr_name, " | ", width(hcr_x), " | ", length(ov), " !!!!!!!!!!!!! ")
-#     } else {
-#       message("##### ", cofactor, " | ", hcr_name, " | ", width(hcr_x), " | ", length(ov))
-#     }
-#   }
-# }
-# 
-# sapply(hcr_vs_genes, length)
-# 
-# saveRDS(hcr_vs_genes, file = "output/analyses/hcr/hcr_vs_genes.rds")
-
-### Work with TAD
-tad_1h_raw <- read_delim(file = "input/ENCODE/A549/GRCh38/hic_TAD/hic_dex.t1.tads.bedpe", delim = "\t")[, 1:3]
-colnames(tad_1h_raw) <- c("seqnames", "start", "end")
-tad_1h <- GRanges(tad_1h_raw)
-
-tad_vs_genes <- list()
-for (cofactor in cofactors) {
+hcr_vs_genes <- list()
+for (cofactor in c("BRD4")) {
   geneup <- genes_reg_by_cofactors_viaLinear[[paste(cofactor, "UP", sep = "_")]]
   tss_up <- stdchr_promoters_A549[geneup]
   tss_up$direction <- "up"
@@ -139,28 +101,78 @@ for (cofactor in cofactors) {
   tss_down <- stdchr_promoters_A549[genedown]
   tss_down$direction <- "down"
 
-  tad_cofactor <- tad_vs_genes[[cofactor]]
+  hcr_cofactor <- hcr_vs_genes[[cofactor]]
 
-  for (i in 1:length(tad_1h)) {
-    tad_name <- paste("TAD", i, sep = "_")
-    tad_x <- tad_1h[i]
+  for (i in 1:length(hcr)) {
+    hcr_name <- paste("HCR", i, sep = "_")
+    hcr_x <- hcr[i] + 500000
 
     ov <- GRanges()
     ov <- c(ov,
-            subsetByOverlaps(tss_up, tad_x),
-            subsetByOverlaps(tss_down, tad_x))
+            subsetByOverlaps(tss_up, hcr_x),
+            subsetByOverlaps(tss_down, hcr_x))
 
     if (length(ov) > 1) {
       names(ov) <- c()
-      tad_vs_genes[[cofactor]][[tad_name]] <- as.data.frame(ov)
-      message("##### ", cofactor, " | ", tad_name, " | ", width(tad_x), " | ", length(ov), " !!!!!!!!!!!!! ")
-    } else {
-      message("##### ", cofactor, " | ", tad_name, " | ", width(tad_x), " | ", length(ov))
+      hcr_vs_genes[[cofactor]][[hcr_name]] <- as.data.frame(ov)
+      message("##### ", cofactor, " | ", hcr_name, " | ", width(hcr_x) - 1000000, " | ", length(ov), " !!!!!!!!!!!!! ")
+    } else if (length(ov) == 1) {
+      message("##### ", cofactor, " | ", hcr_name, " | ", width(hcr_x) - 1000000, " | ", length(ov))
     }
   }
 }
 
-sapply(tad_vs_genes, length)
-saveRDS(tad_vs_genes, file = "output/analyses/hcr/tad_vs_genes_v1.rds")
+sapply(hcr_vs_genes, length)
+
+hcr_brd4 <- hcr_vs_genes$BRD4
+sapply(hcr_brd4, nrow) %>% table
+
+for (elt in names(hcr_brd4)) {
+  df <- hcr_brd4[[elt]]
+  if (nrow(df) > 2) {
+    print(knitr::kable(df))
+  }
+}
+
+# 
+# saveRDS(hcr_vs_genes, file = "output/analyses/hcr/hcr_vs_genes.rds")
+
+### Work with TAD
+# tad_1h_raw <- read_delim(file = "input/ENCODE/A549/GRCh38/hic_TAD/hic_dex.t1.tads.bedpe", delim = "\t")[, 1:3]
+# colnames(tad_1h_raw) <- c("seqnames", "start", "end")
+# tad_1h <- GRanges(tad_1h_raw)
+# 
+# tad_vs_genes <- list()
+# for (cofactor in cofactors) {
+#   geneup <- genes_reg_by_cofactors_viaLinear[[paste(cofactor, "UP", sep = "_")]]
+#   tss_up <- stdchr_promoters_A549[geneup]
+#   tss_up$direction <- "up"
+#   genedown <- genes_reg_by_cofactors_viaLinear[[paste(cofactor, "DOWN", sep = "_")]]
+#   tss_down <- stdchr_promoters_A549[genedown]
+#   tss_down$direction <- "down"
+# 
+#   tad_cofactor <- tad_vs_genes[[cofactor]]
+# 
+#   for (i in 1:length(tad_1h)) {
+#     tad_name <- paste("TAD", i, sep = "_")
+#     tad_x <- tad_1h[i]
+# 
+#     ov <- GRanges()
+#     ov <- c(ov,
+#             subsetByOverlaps(tss_up, tad_x),
+#             subsetByOverlaps(tss_down, tad_x))
+# 
+#     if (length(ov) > 1) {
+#       names(ov) <- c()
+#       tad_vs_genes[[cofactor]][[tad_name]] <- as.data.frame(ov)
+#       message("##### ", cofactor, " | ", tad_name, " | ", width(tad_x), " | ", length(ov), " !!!!!!!!!!!!! ")
+#     } else {
+#       message("##### ", cofactor, " | ", tad_name, " | ", width(tad_x), " | ", length(ov))
+#     }
+#   }
+# }
+# 
+# sapply(tad_vs_genes, length)
+# saveRDS(tad_vs_genes, file = "output/analyses/hcr/tad_vs_genes_v1.rds")
 
 # reperform TAD identification?
